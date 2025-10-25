4b8a262677d9485b68a00d739062222fd78374b3
Babatunde Ishau <ishautunde700@gmail.com>
2025-10-25 10:08:12 +0100

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true'
+// Detect if we're building for GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
 
 export default defineConfig({
   plugins: [react()],
   base: isGitHubPages ? '/React-Ticket-Web-App/' : '/', 
+  build: {
+    outDir: 'docs' 
+  }
 })
