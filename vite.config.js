6d00d3b79bc255e93c16d19edd77e14c43c623f7
Babatunde Ishau <ishautunde700@gmail.com>
2025-10-25 09:49:38 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/React-Ticket-Web-App/' : '/', 
+  base: isGitHubPages ? '/React-Ticket-Web-App/' : '/', 
 })
